c62000d8bc7a66bc239929a6817f676abf7fb467
ElNaj <nelfateoui@gmail.com>
2021-12-14 12:06:46 +0100

    feat: Création du projet backend.

---
+++ backend/santagift/src/main/java/fr/santagift/santagift/SantagiftApplication.java
@@ -0,0 +1,13 @@
+package fr.santagift.santagift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantagiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantagiftApplication.class, args);
+	}
+
+}
---
+++ backend/santagift/src/test/java/fr/santagift/santagift/SantagiftApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.santagift.santagift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantagiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
